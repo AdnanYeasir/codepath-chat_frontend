a1e9e57207b619dff334f3b71bdb13b3ee793e22
AdnanYeasir <106778635+AdnanYeasir@users.noreply.github.com>
2023-11-13 21:34:05 -0500

    Add files via upload

---
+++ src/ChatsPage.jsx
@@ -0,0 +1,14 @@
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced';
+
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic('28993bf0-2bdf-44e9-94be-6a61647ea688', props.user.username, props.user.secret);
+    
+    return (
+        <div style={{ height: '100vh' }}>
+            <MultiChatSocket {...chatProps} />
+            <MultiChatWindow {...chatProps} style={{ height: '100%' }} />
+        </div>
+    );
+}
+
+export default ChatsPage;
